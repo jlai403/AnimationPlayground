1f08946052c1400b8ae20b6c7f4584a9150ef0a9
Joey <j.lai@live.ca>
2015-06-12 20:25:21 -0600

    Added navigation controller

---
+++ AnimationPlayground/HomeController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeController.swift
+//  AnimationPlayground
+//
+//  Created by Joey Lai on 2015-06-12.
+//  Copyright (c) 2015 Joey Lai. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    @IBAction func returnHome(segue: UIStoryboardSegue) {
+    }
+}
